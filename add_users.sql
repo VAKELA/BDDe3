73cd73c3a59bacf56a7fc15cb3316bf007c0995b
JoseTRebolledo <jos.rebolledo@uc.cl>
2021-12-01 14:31:09 -0300

    add user

---
+++ add_users.sql
@@ -0,0 +1,36 @@
+CREATE OR REPLACE FUNCTION
+
+-- declaramos la función y sus argumentos
+add_users()
+
+-- declaramos lo que retorna
+RETURNS void AS $$
+
+-- declaramos las variables a utilizar si es que es necesario
+DECLARE
+
+tupla RECORD;
+largo int;
+horas varchar(60);
+RandomString varchar(20);
+pass varchar(50);
+
+
+-- definimos nuestra función
+BEGIN  -- 
+
+    FOR tupla IN (SELECT usuarios.nombre, SUM(horas_juego.horas) AS horas_jugadas, usuarios.id FROM usuarios, horas_juego WHERE usuarios.id = horas_juego.id_usuarios GROUP BY usuarios.id) 
+    LOOP
+        horas = ((tupla.horas_jugadas::numeric)::varchar)
+        RandomString = array_to_string(ARRAY(SELECT chr((97 + round(random() * 25)) :: integer) FROM generate_series(1,15)), '');
+        pass = MD5(horas || RandomString || tupla.nombre)
+        UPDATE usuarios SET contrasena = pass WHERE usuarios.id = tupla.id
+
+
+
+
+    END LOOP;
+
+-- -- finalizamos la definición de la función y declaramos el lenguaje
+END
+$$ language plpgsql
