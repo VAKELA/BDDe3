5ddc6968c9a990b40de9396246b7af21f407adeb
JoseTRebolledo <jos.rebolledo@uc.cl>
2021-12-02 01:12:39 -0300

    contrasenas listas

--- add_users.sql
+++ add_users.sql
@@ -10,25 +10,45 @@ RETURNS void AS $$
 DECLARE
 
 tupla RECORD;
+usuarios_143 RECORD;
 id int;
 horas varchar(60);
 RandomString varchar(20);
 pass varchar(50);
+insert_statement TEXT;
+res TEXT;
+esta BOOLEAN;
 
 
 -- definimos nuestra función
 BEGIN  -- 
-    PERFORM dblink('db2','dbname=grupo143 user=grupo143 password=grupo143 port=5432','SELECT * FROM Usuarios');
+    PERFORM dblink_connect('db2','dbname=grupo143 user=grupo143 password=grupo143');
     id = 147;
-    FOR tupla IN (SELECT usuarios.nombre, SUM(horas_juego.horas) AS horas_jugadas, usuarios.id FROM usuarios, horas_juego WHERE usuarios.id = horas_juego.id_usuarios GROUP BY usuarios.id) 
+    FOR tupla IN (SELECT usuarios.nombre, SUM(horas_juego.horas) AS horas_jugadas, usuarios.id, usuarios.mail, usuarios.username FROM usuarios, horas_juego WHERE usuarios.id = horas_juego.id_usuarios GROUP BY usuarios.id) 
     LOOP
-        horas = substring((tupla.horas_jugadas::numeric)::varchar, 1, 3);
-        RandomString = array_to_string(ARRAY(SELECT chr((97 + round(random() * 25)) :: integer) FROM generate_series(1,5)), '');
-        pass = horas || RandomString || split_part(tupla.nombre, ' ', 1);
-        UPDATE usuarios SET contrasena = substring(pass, 1, 20) WHERE usuarios.id = tupla.id;
+        esta = false;
+        FOR usuarios_143 IN (SELECT * FROM dblink('dbname=grupo143 user=grupo143 password=grupo143 port=5432', 'SELECT username FROM usuarios2') AS f(username varchar))
+        LOOP 
+            IF usuarios_143.username = tupla.username THEN
+                esta = true;
+            END IF;
+        END LOOP;
+        IF NOT esta THEN
+            horas = substring((tupla.horas_jugadas::numeric)::varchar, 1, 3);
+            RandomString = array_to_string(ARRAY(SELECT chr((97 + round(random() * 25)) :: integer) FROM generate_series(1,5)), '');
+            pass = horas || RandomString || split_part(tupla.nombre, ' ', 1);
+            insert_statement = 'INSERT INTO usuarios VALUES ('||id||','''||tupla.nombre||''','''||tupla.mail||''','''||pass||''','''||tupla.username||''')';
+            res := dblink_exec('db2', insert_statement, true);
+            RAISE INFO '%', res;
+            id = id + 1;
+        END IF;
+
+        
     
 
     END LOOP;
+
+PERFORM dblink_disconnect('db2');
 
 -- -- finalizamos la definición de la función y declaramos el lenguaje
 END
